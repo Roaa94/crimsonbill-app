4824b0f57acb0e69406d37a3f4b51830ff8683d3
roaakh <roaa@roperla.com>
2020-03-03 16:34:43 +0300

    customized app button component

--- src/components/ui/AppButton.js
+++ src/components/ui/AppButton.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => {
+    return {
+        primary: {
+            border: `1px solid transparent`,
+            '&:hover': {
+                backgroundColor: '#ffff',
+                color: theme.palette.primary.main,
+                border: `1px solid ${theme.palette.primary.main}`,
+            }
+        },
+        secondary: {
+            border: `1px solid transparent`,
+            '&:hover': {
+                backgroundColor: '#ffff',
+                color: theme.palette.secondary.main,
+                border: `1px solid ${theme.palette.secondary.main}`,
+            }
+        }
+    }
+});
 
 const AppButton = ({children, color, fullWidth = true, ...otherProps}) => {
+    const classes = useStyles();
     return (
         <Button
+            className={color === 'secondary' ? classes.secondary : classes.primary}
             variant='contained'
             color={color ? color : 'primary'}
             disableElevation
