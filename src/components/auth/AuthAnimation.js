86a0ba19aaf198b1d362f8911eef165e4d60a164
roaakh <roaa@roperla.com>
2020-04-11 15:29:18 +0300

    replaced auth artwork

--- src/components/auth/AuthAnimation.js
+++ src/components/auth/AuthAnimation.js
@@ -1,52 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import {ReactComponent as AuthArtwork} from '../../assets/svg/woman-working.svg';
+import {ReactComponent as AuthArtwork} from '../../assets/svg/auth-artwork.svg';
 
-const AnimationWrapper = styled.div`
-  .head{
-    transform-origin: 988px 327px;
-    animation: head 3s infinite ease-in-out;
-  }
-  
-  @keyframes head{
-    50%{transform: rotate(4deg);}
-  }
-  
-  .arm-2{
-    transform-origin: 1297px 615px;
-    animation: arm 1s infinite ease-out;
-    animation-play-state: ${props => props.animationState ? 'paused' : 'running'};
-  }
-  
-  @keyframes arm{
-    50%{transform: rotate(3deg);}
-  }
-  
-  .front-foot{
-    transform-origin: 988px 1686px;
-    animation: foot 1s infinite ease-in-out;
-  }
-  
-  @keyframes foot{
-    50%{transform: rotate(5deg);}
-  }
-`;
+const AnimationWrapper = styled.div``;
 
 class AuthAnimation extends React.Component {
-    state = {
-        pauseAnimation: true,
-    };
-
-    toggleAnimation = () => {
-        this.setState({pauseAnimation: !this.state.pauseAnimation})
-    };
 
     render() {
-        const {pauseAnimation} = this.state;
-
         return (
-            <AnimationWrapper animationState={pauseAnimation}>
-                {/*<span onClick={this.toggleAnimation}>Toggle animation</span>*/}
+            <AnimationWrapper>
                 <AuthArtwork/>
             </AnimationWrapper>
         );
