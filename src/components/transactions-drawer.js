f3d8bf13cca3cce58d599e2a8378a5080821896c
roaakh <roaa@roperla.com>
2020-04-18 13:22:01 +0300

    test commit 2

--- src/components/transactions-drawer.js
+++ src/components/transactions-drawer.js
@@ -39,7 +39,7 @@ const TransactionsDrawer = ({open, currentUser}) => {
         >
             <div>
                 <h3>Latest Transactions</h3>
-                <p>No transactions to displayyyy</p>
+                <p>No transactions to display</p>
             </div>
             <FolderCabinetSVG className={classes.svgImage}/>
         </Drawer>
