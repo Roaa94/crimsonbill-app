3854342483542c5272034c2aadcf134651a6a0c8
Roaa94 <roaa94khaddam@gmail.com>
2020-04-25 17:23:35 +0300

    set up account card expansion panel basic styling

--- src/components/accounts/account-card/AccountCard.component.js
+++ src/components/accounts/account-card/AccountCard.component.js
@@ -4,6 +4,9 @@ import {createStructuredSelector} from "reselect";
 import {selectUser} from "../../../redux/user/user.selectors";
 import {selectAccountFormShow} from "../../../redux/account-form/account-form.selectors";
 import {connect} from "react-redux";
+import {ExpansionPanel, ExpansionPanelHeader, ExpansionPanelContent} from './AccountCard.styles';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import AccountBalanceRoundedIcon from '@material-ui/icons/AccountBalanceRounded';
 
 class AccountCard extends React.Component {
 
@@ -16,14 +19,22 @@ class AccountCard extends React.Component {
         let {id, type, name, currency, notes} = this.props;
 
         return (
-            <div>
-                <p>Type: {type}</p>
-                <p>Name: {name}</p>
-                <p>Currency: {currency}</p>
-                <p>Notes: {notes}</p>
-                <button onClick={() => this.deleteAccount(id)}>Delete</button>
-                <hr/>
-            </div>
+            <ExpansionPanel>
+                <ExpansionPanelSummary>
+                    <ExpansionPanelHeader>
+                        <h3>{name}</h3>
+                        <h3 className='account-currency'>{currency}</h3>
+                        <div className='account-type'>
+                            <AccountBalanceRoundedIcon/>
+                            {type}
+                        </div>
+                    </ExpansionPanelHeader>
+                </ExpansionPanelSummary>
+                <ExpansionPanelContent>
+                    {notes}
+                    <button onClick={() => this.deleteAccount(id)}>Delete</button>
+                </ExpansionPanelContent>
+            </ExpansionPanel>
         );
     }
 }
---
+++ src/components/accounts/account-card/AccountCard.styles.js
@@ -0,0 +1,55 @@
+import {withStyles} from "@material-ui/core/styles";
+import MuiExpansionPanel from "@material-ui/core/ExpansionPanel";
+import styled from 'styled-components';
+import {colors} from "../../../styles/global";
+
+export const ExpansionPanel = withStyles({
+    root: {
+        margin: '30px 0',
+        borderRadius: '15px !important',
+        boxShadow: 'none',
+        borderBottom: '0 !important',
+        transition: 'box-shadow .3s',
+        '&:before': {
+            display: 'none',
+        },
+        '&$expanded': {
+            margin: '30px 0 !important',
+        },
+        '&:hover': {
+            boxShadow: '0 0 15px rgba(0,0,0,0.1)',
+        }
+    },
+    expanded: {},
+})(MuiExpansionPanel);
+
+export const ExpansionPanelHeader = styled.div`
+  padding: 5px 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  
+  h3{
+    margin: 0 !important;
+  }
+  
+  .account-currency{
+    color: ${colors.info};
+  }
+  
+  .account-type{
+    color: ${colors.secondary};
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    
+    svg{
+      margin-right: 10px;
+    }
+  }
+`;
+
+export const ExpansionPanelContent = styled.div`
+  padding: 20px;
+`;
