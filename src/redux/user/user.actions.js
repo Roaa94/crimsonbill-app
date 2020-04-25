973ee98842f87cf934c5fe4604af1d89c19c60f6
Roaa94 <roaa94khaddam@gmail.com>
2020-04-25 17:54:58 +0300

    fixed user reducer state syntax and removed unused actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,13 +4,3 @@ export const setUser = user => ({
     type: userActionTypes.SET_CURRENT_USER,
     payload: user
 });
-
-export const updateUserAvatar = avatarUrl => ({
-    type: userActionTypes.UPDATE_USER_AVATAR,
-    payload: avatarUrl,
-});
-
-export const updateUserAccounts = accounts => ({
-    type: userActionTypes.UPDATE_USER_ACCOUNTS,
-    payload: accounts,
-});
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -3,20 +3,7 @@ import {userActionTypes} from "./user.action-types";
 const userReducer = (state = null, action) => {
     switch (action.type) {
         case userActionTypes.SET_CURRENT_USER:
-            return {
-                ...state,
-                ...action.payload
-            };
-        case userActionTypes.UPDATE_USER_AVATAR:
-            return {
-                ...state,
-                avatarUrl: action.payload
-            };
-        case userActionTypes.UPDATE_USER_ACCOUNTS:
-            return {
-                ...state,
-                accounts: action.payload
-            };
+            return action.payload;
         default:
             return state;
     }
