3723b6e59ff50d3283030433a5651935e978ef3a
Roaa94 <roaa94khaddam@gmail.com>
2020-04-26 04:49:18 +0300

    worked on account form grid and currency and type dropdowns

--- src/components/accounts/account-form/AccountForm.component.js
+++ src/components/accounts/account-form/AccountForm.component.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {addOrUpdateUserAccountDocument} from "../../../firebase/accounts.utils";
-import TextFieldOutline from "../../ui/TextFieldOutline";
-import Button from "../../ui/Button";
+import TextFieldFilled from "../../ui/inputs/TextFieldFilled";
+import Button from "../../ui/buttons/Button";
 import {selectUser} from "../../../redux/user/user.selectors";
 import {connect} from "react-redux";
 import {firestore} from "../../../firebase/firebase.utils";
@@ -13,6 +13,10 @@ import {
 } from "./AccountForm.styles";
 import {createStructuredSelector} from "reselect";
 import {selectAccountFormShow} from "../../../redux/account-form/account-form.selectors";
+import Grid from '@material-ui/core/Grid';
+import Box from "@material-ui/core/Box";
+import DropDown from "../../ui/navigation/drop-down/DropDown.component";
+import {currencies, types} from "../../../data";
 
 class AccountForm extends Component {
     state = {
@@ -50,11 +54,10 @@ class AccountForm extends Component {
         toggleAccountForm(false);
     };
 
-    handleTextFieldChange = event => {
+    handleFieldChange = event => {
         const {name, value} = event.target;
         this.setState({[name]: value});
     };
-
 
     render() {
         const {type, name, currency, notes} = this.state;
@@ -66,34 +69,52 @@ class AccountForm extends Component {
                 <AccountFormExpansionPanelContent>
                     <h3>Add Account</h3>
                     <form onSubmit={this.handleFormSubmit}>
-                        <TextFieldOutline
-                            label='Account Type'
-                            value={type}
-                            name='type'
-                            type='text'
-                            onChange={this.handleTextFieldChange}
-                        />
-                        <TextFieldOutline
-                            label='Account Name'
-                            value={name}
-                            name='name'
-                            type='text'
-                            onChange={this.handleTextFieldChange}
-                        />
-                        <TextFieldOutline
-                            label='Account Currency'
-                            value={currency}
-                            name='currency'
-                            type='text'
-                            onChange={this.handleTextFieldChange}
-                        />
-                        <TextFieldOutline
-                            label='Notes'
-                            value={notes}
-                            name='notes'
-                            type='text'
-                            onChange={this.handleTextFieldChange}
-                        />
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={6} lg={4}>
+                                <TextFieldFilled
+                                    label='Account Name'
+                                    value={name}
+                                    name='name'
+                                    type='text'
+                                    onChange={this.handleFieldChange}
+                                />
+                            </Grid>
+                            <Grid item xs={6} md={3} lg={2}>
+                                <DropDown
+                                    label='Type'
+                                    name='type'
+                                    value={type}
+                                    menuItems={types}
+                                    onChange={this.handleFieldChange}
+                                />
+                            </Grid>
+                            <Grid item xs={6} md={3} lg={2}>
+                                <DropDown
+                                    label='Currency'
+                                    name='currency'
+                                    value={currency}
+                                    menuItems={currencies}
+                                    onChange={this.handleFieldChange}
+                                />
+                            </Grid>
+                            <Grid item xs={6} lg={2}>
+                                {/* Add Balance Button */}
+                            </Grid>
+                            <Grid item xs={6} lg={2}>
+                                {/*  Add Transaction Button  */}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Box mb={2}>
+                                    <TextFieldFilled
+                                        label='Notes'
+                                        value={notes}
+                                        name='notes'
+                                        type='text'
+                                        onChange={this.handleFieldChange}
+                                    />
+                                </Box>
+                            </Grid>
+                        </Grid>
                         <Button type='submit'>{accountId ? 'Edit Account' : 'Add Account'}</Button>
                         <Button onClick={() => toggleAccountForm(false)}>Cancel</Button>
                     </form>
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export const types = [
+    {
+        id: 0,
+        title: 'Type-1',
+        value: 'Type-1',
+    },
+    {
+        id: 1,
+        title: 'Type-2',
+        value: 'Type-2',
+    },
+    {
+        id: 3,
+        title: 'Type-3',
+        value: 'Type-3',
+    },
+];
+
+export const currencies = [
+    {
+        id: 0,
+        title: '$',
+        value: '$',
+    },
+    {
+        id: 1,
+        title: '$',
+        value: '$',
+    },
+    {
+        id: 3,
+        title: '$',
+        value: '$',
+    },
+];
