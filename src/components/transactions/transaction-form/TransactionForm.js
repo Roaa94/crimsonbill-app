4d90f2f16716b8c553072edbcc34cb039c578c0f
Roaa94 <roaa94khaddam@gmail.com>
2020-05-01 23:09:30 +0300

    refactored transaction form (separated layout from state)

--- src/components/transactions/transaction-form/TransactionForm.js
+++ src/components/transactions/transaction-form/TransactionForm.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import Grid from "@material-ui/core/Grid";
-import TransactionTypeIcon from "../TransactionTypeIcon";
 import Button from "../../ui/buttons/Button";
 import {colors} from "../../../styles/global";
 import CheckRoundedIcon from "@material-ui/icons/CheckRounded";
 import ClearRoundedIcon from "@material-ui/icons/ClearRounded";
 import {categories} from "../../../data";
-import Select from "../../ui/inputs/Select";
-import Box from "@material-ui/core/Box";
-import TextField from "@material-ui/core/TextField";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import DateTimePicker from "../../ui/inputs/date-time-pickers/DateTimePicker";
-import Icon from "@material-ui/core/Icon";
-import DoubleArrowRoundedIcon from '@material-ui/icons/DoubleArrowRounded';
 import {addOrUpdateTransactionDocument} from "../../../firebase/transactions.firebase-utils";
 import {createStructuredSelector} from "reselect";
 import {selectUserId} from "../../../redux/user/user.selectors";
 import {connect} from "react-redux";
+import TransactionFormLayout from "./TransactionFormLayout";
 
 class TransactionForm extends React.Component {
     defaultTransactionValues = {
@@ -31,14 +23,14 @@ class TransactionForm extends React.Component {
         notes: '',
     };
 
-    typePicker = {
+    typePickerValues = {
         spending: true,
         earning: false,
     };
 
     state = {
         defaultValues: this.defaultTransactionValues,
-        typePicker: this.typePicker,
+        typePickerValues: this.typePickerValues,
     };
 
     handleFormSubmit = async event => {
@@ -48,7 +40,7 @@ class TransactionForm extends React.Component {
         await addOrUpdateTransactionDocument(userId, accountId, balanceId, transactionId, transactionData);
         this.setState({
             defaultValues: this.defaultTransactionValues,
-            typePicker: this.typePicker,
+            typePickerValues: this.typePickerValues,
         });
         handleFormCancel();
     }
@@ -71,18 +63,19 @@ class TransactionForm extends React.Component {
                 ...defaultValues,
                 type: value,
             },
-            typePicker: {
+            typePickerValues: {
                 [value]: true,
             }
         })
     };
 
-    handleDateChange = pickedDate => {
+    handleDateTimeChange = pickedDate => {
+        console.log(pickedDate);
         const {defaultValues} = this.state;
         this.setState({
             defaultValues: {
                 ...defaultValues,
-                date: pickedDate,
+                dateTime: pickedDate,
             }
         });
     }
@@ -98,120 +91,30 @@ class TransactionForm extends React.Component {
     }
 
     render() {
-        const {typePicker, defaultValues} = this.state;
+        const {typePickerValues, defaultValues} = this.state;
         const {category, amount, dateTime, accountToAccount, notes, type} = defaultValues;
         const {handleFormCancel} = this.props;
+        console.log(defaultValues);
 
         return (
             <form onSubmit={this.handleFormSubmit}>
-                <Box mb={2}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <Box fontWeight='600' fontSize='small'>
-                                Add Transaction
-                            </Box>
-                        </Grid>
-                        <Grid item container alignItems='stretch' wrap='nowrap' xs={12} md={7} xl={4}>
-                            <Grid item>
-                                <TransactionTypeIcon
-                                    type='spending'
-                                    checked={typePicker.spending}
-                                    onClick={() => this.handleTypePickerChange('spending')}
-                                />
-                            </Grid>
-                            <Grid item>
-                                <TransactionTypeIcon
-                                    type='earning'
-                                    checked={typePicker.earning}
-                                    onClick={() => this.handleTypePickerChange('earning')}
-                                />
-                            </Grid>
-                            <Grid item xs>
-                                <Select
-                                    label='Category'
-                                    name='category'
-                                    value={category}
-                                    menuItems={categories}
-                                    onChange={this.handleFieldChange}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} md={5} xl={2}>
-                            <TextField
-                                label='Amount'
-                                value={amount}
-                                name='amount'
-                                type='text'
-                                required
-                                onChange={this.handleFieldChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12} xl={6}>
-                            <DateTimePicker
-                                selectedDate={dateTime}
-                                dateFieldLabel='Transaction Date'
-                                timeFieldLabel='Transaction Time'
-                                onChange={this.handleDateChange}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        checked={accountToAccount}
-                                        onChange={this.handleCheckBoxChange}
-                                        name="accountToAccount"
-                                        color={type === 'spending' ? 'primary' : 'secondary'}
-                                    />
-                                }
-                                label="Account to Account Transaction"
-                            />
-                        </Grid>
-                        {
-                            accountToAccount ? (
-                                <Grid item xs={12} container alignItems='center' justify='space-between'>
-                                    <Grid item xs={12} md={5}>
-                                        <Select
-                                            label='From Account'
-                                            name='category'
-                                            value={category}
-                                            menuItems={categories}
-                                            onChange={this.handleFieldChange}
-                                        />
-                                    </Grid>
-                                    <Grid container item xs={12} md justify='center'>
-                                        <Icon
-                                            color={type === 'spending' ? 'primary' : 'secondary'}
-                                            fontSize='large'
-                                        >
-                                            <DoubleArrowRoundedIcon/>
-                                        </Icon>
-                                    </Grid>
-                                    <Grid item xs={12} md={5}>
-                                        <Select
-                                            label='To Account'
-                                            name='category'
-                                            value={category}
-                                            menuItems={categories}
-                                            onChange={this.handleFieldChange}
-                                        />
-                                    </Grid>
-                                </Grid>
-                            ) : null
-                        }
-                        <Grid item xs={12}>
-                            <TextField
-                                label='Transaction Notes'
-                                value={notes}
-                                name='notes'
-                                type='text'
-                                multiline
-                                rows={2}
-                                onChange={this.handleFieldChange}
-                            />
-                        </Grid>
-                    </Grid>
-                </Box>
+                <TransactionFormLayout
+                    typePickers={typePickerValues}
+                    onSelectType={this.handleTypePickerChange}
+                    onFieldChange={this.handleFieldChange}
+                    categorySelectValue={category}
+                    categorySelectItems={categories}
+                    amountValue={amount}
+                    dateTimeValue={dateTime}
+                    onDateTimeChange={this.handleDateTimeChange}
+                    accountToAccount={accountToAccount}
+                    onCheckboxChange={this.handleCheckBoxChange}
+                    type={type}
+                    fromAccountSelectValue={category}
+                    toAccountSelectValue={category}
+                    accountList={categories}
+                    notesValue={notes}
+                />
                 <Grid container>
                     <Button
                         fullWidth={false}
---
+++ src/components/transactions/transaction-form/TransactionFormLayout.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import TransactionTypeIcon from "../TransactionTypeIcon";
+import Icon from "@material-ui/core/Icon";
+import DoubleArrowRoundedIcon from "@material-ui/icons/DoubleArrowRounded";
+import Select from "../../ui/inputs/Select";
+import TextField from "@material-ui/core/TextField";
+import DateTimePicker from "../../ui/inputs/date-time-pickers/DateTimePicker";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+const TransactionFormLayout = (
+    {
+        typePickers,
+        onSelectType,
+        categorySelectValue,
+        categorySelectItems,
+        amountValue,
+        dateTimeValue,
+        accountToAccount,
+        onCheckboxChange,
+        fromAccountSelectValue,
+        toAccountSelectValue,
+        type,
+        notesValue,
+        onFieldChange,
+        onDateTimeChange,
+        accountList
+    }
+) => {
+    return (
+        <Box mb={2}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Box fontWeight='600' fontSize='small'>
+                        Add Transaction
+                    </Box>
+                </Grid>
+                <Grid item container alignItems='stretch' wrap='nowrap' xs={12} md={7} xl={4}>
+                    <Grid item>
+                        <TransactionTypeIcon
+                            type='spending'
+                            checked={typePickers.spending}
+                            onClick={() => onSelectType('spending')}
+                        />
+                    </Grid>
+                    <Grid item>
+                        <TransactionTypeIcon
+                            type='earning'
+                            checked={typePickers.earning}
+                            onClick={() => onSelectType('earning')}
+                        />
+                    </Grid>
+                    <Grid item xs>
+                        <Select
+                            label='Category'
+                            name='category'
+                            value={categorySelectValue}
+                            menuItems={categorySelectItems}
+                            onChange={onFieldChange}
+                        />
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} md={5} xl={2}>
+                    <TextField
+                        label='Amount'
+                        value={amountValue}
+                        name='amount'
+                        type='text'
+                        required
+                        onChange={onFieldChange}
+                    />
+                </Grid>
+                <Grid item xs={12} xl={6}>
+                    <DateTimePicker
+                        selectedDate={dateTimeValue}
+                        dateFieldLabel='Transaction Date'
+                        timeFieldLabel='Transaction Time'
+                        onChange={onDateTimeChange}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={accountToAccount}
+                                onChange={onCheckboxChange}
+                                name="accountToAccount"
+                                color={type === 'spending' ? 'primary' : 'secondary'}
+                            />
+                        }
+                        label="Account to Account Transaction"
+                    />
+                </Grid>
+                {
+                    accountToAccount ? (
+                        <Grid item xs={12} container alignItems='center' justify='space-between'>
+                            <Grid item xs={12} md={5}>
+                                <Select
+                                    label='From Account'
+                                    name='category'
+                                    value={fromAccountSelectValue}
+                                    menuItems={accountList}
+                                    onChange={onFieldChange}
+                                />
+                            </Grid>
+                            <Grid container item xs={12} md justify='center'>
+                                <Icon
+                                    color={type === 'spending' ? 'primary' : 'secondary'}
+                                    fontSize='large'
+                                >
+                                    <DoubleArrowRoundedIcon/>
+                                </Icon>
+                            </Grid>
+                            <Grid item xs={12} md={5}>
+                                <Select
+                                    label='To Account'
+                                    name='category'
+                                    value={toAccountSelectValue}
+                                    menuItems={accountList}
+                                    onChange={onFieldChange}
+                                />
+                            </Grid>
+                        </Grid>
+                    ) : null
+                }
+                <Grid item xs={12}>
+                    <TextField
+                        label='Transaction Notes'
+                        value={notesValue}
+                        name='notes'
+                        type='text'
+                        multiline
+                        rows={2}
+                        onChange={onFieldChange}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default TransactionFormLayout;
