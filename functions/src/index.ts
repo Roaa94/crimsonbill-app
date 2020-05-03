6ea9693266ed34e2a7f939abcbb649e711fc5cee
Roaa94 <roaa94khaddam@gmail.com>
2020-05-03 06:03:00 +0300

    added firebase function to delete subcollections of account on account deletion

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,13 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
+const db = admin.firestore();
+
+//Todo: configure the bellow batch writes for collections with +500 documents
 
 export const deleteBalanceSubCollections = functions.firestore
     .document('users/{userId}/accounts/{accountId}/balances/{balanceId}')
     .onDelete((snapshot, context) => {
-        const db = admin.firestore();
         const batch = db.batch();
         const transactionsPath = `users/${context.params.userId}/accounts/${context.params.accountId}/balances/${context.params.balanceId}/transactions`;
         db.collection(transactionsPath).get()
@@ -16,8 +18,32 @@ export const deleteBalanceSubCollections = functions.firestore
                 })
                 return batch.commit();
             })
-            .catch(error => error.measure())
+            .catch(error => error.message)
         return null;
+    })
+
+export const deleteAccountSubCollections = functions.firestore
+    .document('users/{userId}/accounts/{accountId}')
+    .onDelete((snapshot, context) => {
+        const batch = db.batch();
+        const balancesPath = `users/${context.params.userId}/accounts/${context.params.accountId}/balances`;
+        const transactionsPath = `users/${context.params.userId}/accounts/${context.params.accountId}/transactions`;
+        db.collection(balancesPath).get()
+            .then(balancesSnapshot => {
+                balancesSnapshot.docs.forEach(doc => {
+                    batch.delete(doc.ref);
+                })
+            })
+            .then(() => {
+                db.collection(transactionsPath).get()
+                    .then(transactionsSnapshot => {
+                        transactionsSnapshot.docs.forEach(doc => {
+                            batch.delete(doc.ref);
+                        })
+                        return batch.commit()
+                    })
+                    .catch(error => error.message)
+            }).catch(error => error.message)
     })
 
 //The function works fine but it makes the onSnapshot listener fire a lot of time and it's too slow
