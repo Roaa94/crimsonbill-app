21f016a7fb98454352e8d4044350ea33caab6e60
Roaa94 <roaa94khaddam@gmail.com>
2020-05-14 19:14:48 +0300

    added currency reducer files

---
+++ src/redux/currency/currency.reducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+    defaultCurrency: null,
+    appCurrencies: [],
+};
+
+export const currencyReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import {persistReducer} from "redux-persist";
 import {combineReducers} from "redux";
 import accountsReducer from "./accounts/accounts.reducer";
 import {taxonomiesReducer} from "./taxonomies/taxonomies.reducer";
+import {currencyReducer} from "./currency/currency.reducer";
 
 const persistConfig = {
     key: 'root',
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
     user: userReducer,
     accounts: accountsReducer,
     taxonomies: taxonomiesReducer,
+    currency: currencyReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
