2cef7f7d7a8416c3aba23f38e40c91a182c35d82
Roaa94 <roaa94khaddam@gmail.com>
2020-05-17 21:51:42 +0300

    edit on withLoader HOC styling

--- src/components/HOC/WithLoader.js
+++ src/components/HOC/WithLoader.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 
 const DefaultLoaderWrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
