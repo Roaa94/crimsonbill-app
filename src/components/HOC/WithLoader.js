967d83b4fd913c227bfd1951ac74b1d1939720fa
Roaa94 <roaa94khaddam@gmail.com>
2020-05-18 06:02:15 +0300

    optimized WithLoader HOC

--- src/components/HOC/WithLoader.js
+++ src/components/HOC/WithLoader.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {ReactComponent as Loader} from '../../assets/svg/loader.svg';
+import {ReactComponent as CircleLoader} from '../../assets/svg/loader.svg';
+import {ReactComponent as DotsLoader} from '../../assets/svg/loader-h.svg';
 import styled from 'styled-components';
 
 const DefaultLoaderWrapper = styled.div`
@@ -10,11 +11,13 @@ const DefaultLoaderWrapper = styled.div`
   align-items: center;
 `;
 
-const WithLoader = (WrappedComponent, LoaderWrapper = DefaultLoaderWrapper) => ({loading = false, ...otherProps}) => {
+const WithLoader = (WrappedComponent, LoaderWrapper = DefaultLoaderWrapper) => ({loading = false, type, ...otherProps}) => {
 
     return loading ? (
         <LoaderWrapper>
-            <Loader/>
+            {
+                type === 'dots' ? <DotsLoader/> : <CircleLoader/>
+            }
         </LoaderWrapper>
     ) : <WrappedComponent {...otherProps} />;
 
