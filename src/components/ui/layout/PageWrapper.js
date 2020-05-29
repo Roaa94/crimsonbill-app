a568d7956b2fda41df68794c5855ddda820451be
Roaa94 <roaa94khaddam@gmail.com>
2020-05-29 22:13:06 +0300

    changes on page wrapper component style

--- src/components/ui/layout/PageWrapper.js
+++ src/components/ui/layout/PageWrapper.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import {borderRadius, colors} from "../../../styles/global";
-import {drawerWidth} from "../navigation/Drawer";
 
 const pageMargin = 10;
 
 const PageWrapperDiv = styled.div`
-  position: relative;
   padding: 20px 30px 20px 20px;
-  height: 100%;
-  
-  ::before{
-    content: '';
-    border-radius: ${borderRadius.xl};
-    background-color: ${colors.background};
-    position: fixed;
-    width: calc(100% - ${pageMargin}px - ${drawerWidth}px);
-    height: calc(100% - ${pageMargin * 2}px);
-    top: ${pageMargin}px;
-    right: ${pageMargin}px;
-    bottom: ${pageMargin}px;
-    left: ${drawerWidth}px;
-    z-index: -1;
+  min-height: 100%;
+  border-radius: ${borderRadius.xl};
+  background-color: ${colors.background};
+  margin: ${pageMargin}px ${pageMargin}px ${pageMargin}px 0; 
 `;
 
 const PageWrapper = ({children}) => {
