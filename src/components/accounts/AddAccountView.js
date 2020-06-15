722d9dccb9c439386f2aa4f88d2d69159adf9986
Roaa94 <roaa94khaddam@gmail.com>
2020-06-15 13:57:17 +0300

    fixed add account view button alignment

--- src/components/accounts/AddAccountView.js
+++ src/components/accounts/AddAccountView.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from "styled-components";
 import AddAccountIcon from "./AddAccountIcon";
+import {pageMargin, pagePadding} from "../../styles/global";
 
 export const AddAccountViewWrapper = styled.div`
   width: 100%;
   transition: width 225ms cubic-bezier(0, 0, 0.2, 1) 0ms;
-  height: 100%;
+  height: calc(100vh - ${pageMargin * 2 + pagePadding * 2}px);
   display: flex;
   flex-grow: 1;
   flex-direction: column;
@@ -16,7 +17,7 @@ export const AddAccountViewWrapper = styled.div`
 const AddAccountView = () => {
     return (
         <AddAccountViewWrapper>
-            <AddAccountIcon />
+            <AddAccountIcon/>
             <p>Please add an account to get started</p>
         </AddAccountViewWrapper>
     );
