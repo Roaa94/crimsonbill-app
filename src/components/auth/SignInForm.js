3343528ff2677ce65255d2423a2d859a1e94fc4b
Roaa94 <roaa94khaddam@gmail.com>
2020-09-03 00:11:14 +0300

    converted sing in form to a functional component

--- src/components/auth/SignInForm.js
+++ src/components/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import TextFieldOutlined from "../ui/inputs/TextFieldOutlined";
 import Grid from "@material-ui/core/Grid";
@@ -6,24 +6,26 @@ import Box from "@material-ui/core/Box";
 import ButtonOutlined from "../ui/buttons/ButtonOutlined";
 import {auth, signInWithGoogle} from "../../utils/firebase/firebase.utils";
 
-class SignInForm extends React.Component {
-    state = {
+const SignInForm = () => {
+
+    const [userCredentials, setUserCredentials] = useState({
         email: '',
         password: ''
-    };
+    });
 
-    handleChange = event => {
+    const {email, password} = userCredentials;
+
+    const handleChange = event => {
         const {value, name} = event.target;
-        this.setState({[name]: value});
+        setUserCredentials({...userCredentials, [name]: value});
     };
 
-    handleSubmit = async event => {
+    const handleSubmit = async event => {
         event.preventDefault();
-        const {email, password} = this.state;
 
         try {
             await auth.signInWithEmailAndPassword(email, password);
-            this.setState({
+            setUserCredentials({
                 email: '',
                 password: ''
             });
@@ -32,49 +34,46 @@ class SignInForm extends React.Component {
         }
     };
 
-    render() {
-        const {email, password} = this.state;
 
-        return (
-            <div>
-                <h1>I Already Have An Account</h1>
-                <p>Sign in with email and password</p>
-                <form onSubmit={this.handleSubmit}>
-                    <TextFieldOutlined
-                        label='Email'
-                        name='email'
-                        value={email}
-                        type='text'
-                        onChange={this.handleChange}
-                        required
-                    />
-                    <TextFieldOutlined
-                        label='Password'
-                        name='password'
-                        value={password}
-                        type='password'
-                        onChange={this.handleChange}
-                        required
-                    />
-                    <Box mb={2}>
-                        <Grid container justify='space-between' spacing={2}>
-                            <Grid item xs={12} md={4}>
-                                <ButtonOutlined type='submit'>SIGN IN</ButtonOutlined>
-                            </Grid>
-                            <Grid item xs={12} md={8}>
-                                <ButtonOutlined color='secondary' onClick={signInWithGoogle}>
-                                    SIGN IN WITH GOOGLE
-                                </ButtonOutlined>
-                            </Grid>
+    return (
+        <div>
+            <h1>I Already Have An Account</h1>
+            <p>Sign in with email and password</p>
+            <form onSubmit={handleSubmit}>
+                <TextFieldOutlined
+                    label='Email'
+                    name='email'
+                    value={email}
+                    type='text'
+                    onChange={handleChange}
+                    required
+                />
+                <TextFieldOutlined
+                    label='Password'
+                    name='password'
+                    value={password}
+                    type='password'
+                    onChange={handleChange}
+                    required
+                />
+                <Box mb={2}>
+                    <Grid container justify='space-between' spacing={2}>
+                        <Grid item xs={12} md={4}>
+                            <ButtonOutlined type='submit'>SIGN IN</ButtonOutlined>
                         </Grid>
-                    </Box>
-                </form>
-                <Link to='/sign-up'>
-                    Don't have an account? Sign up
-                </Link>
-            </div>
-        );
-    }
+                        <Grid item xs={12} md={8}>
+                            <ButtonOutlined color='secondary' onClick={signInWithGoogle}>
+                                SIGN IN WITH GOOGLE
+                            </ButtonOutlined>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </form>
+            <Link to='/sign-up'>
+                Don't have an account? Sign up
+            </Link>
+        </div>
+    );
 }
 
 export default SignInForm;
